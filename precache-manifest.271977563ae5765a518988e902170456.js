8ff633c3b1689bd7fee2cb78d8c7af9a896eaaff
Roberto Ribeiro <robertov82008@gmail.com>
2021-05-08 15:56:42 -0300

    Updates

---
+++ precache-manifest.271977563ae5765a518988e902170456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c5c23bc8c18c2530cdc3d8d698835b",
+    "url": "/google-classroom-share/index.html"
+  },
+  {
+    "revision": "1b66064923b59c1a316d",
+    "url": "/google-classroom-share/static/css/main.49c119db.chunk.css"
+  },
+  {
+    "revision": "9d624cb33d39a484f135",
+    "url": "/google-classroom-share/static/js/2.acaee8b6.chunk.js"
+  },
+  {
+    "revision": "1b66064923b59c1a316d",
+    "url": "/google-classroom-share/static/js/main.8de6f732.chunk.js"
+  },
+  {
+    "revision": "086d1a181949782de9dd",
+    "url": "/google-classroom-share/static/js/runtime~main.01f3bd6f.js"
+  }
+]);
